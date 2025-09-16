04de7dbe49fabd5910b78cde1bbc4fee00005dc1
Alex Rotari <arotari@noction.com>
2025-09-16 16:03:58 +0300

    initial commit

---
+++ hello/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Prints "Hello World" to the console.
+ */
+int main()
+{
+    cout << "Hello Programming!\n";
+    cout << "Here we go!\n";
+    return 0;
+}
