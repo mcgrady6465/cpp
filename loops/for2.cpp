244f3876b208a991a5c4e57fb2b03d780d253fb1
Alex Rotari <arotari@noction.com>
2025-09-22 10:06:37 +0300

    adding new exercises for loops2

--- loops/for2.cpp
+++ loops/for2.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+/**
+ * @brief Calculates the square of a given integer x.
+ * @param x The value to square.
+ * @return The square of x.
+ */
 int square(int x) {
     int result = 0;
     for (int i = 0; i < x; ++i) {
